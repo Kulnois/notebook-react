1aaa1c044bea6b762c69fdd357328e30c660bffc
Kulnois <rasecmen58@hotmail.com>
2020-10-02 11:58:52 -0500

    Navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from 'react'
+
+import Navbar from './layout/navbar'
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/layout/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <nav className="blue-grey darken-4">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Notebook</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
