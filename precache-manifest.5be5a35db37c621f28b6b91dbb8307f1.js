13cc04701398acf28ca69ed323fb1f71a9b3b244
Kulnois <rasecmen58@hotmail.com>
2020-10-02 18:14:07 -0500

    Updates

---
+++ precache-manifest.5be5a35db37c621f28b6b91dbb8307f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce94c0466d08a6a26a0e2ef5466ae299",
+    "url": "/notebook-react/index.html"
+  },
+  {
+    "revision": "0c88e642f20b40843dcd",
+    "url": "/notebook-react/static/css/main.86630ecb.chunk.css"
+  },
+  {
+    "revision": "096aeab8b184cc4a0086",
+    "url": "/notebook-react/static/js/2.fafcd3f5.chunk.js"
+  },
+  {
+    "revision": "94efaf9ef8dfdcc9b58d075c8086b235",
+    "url": "/notebook-react/static/js/2.fafcd3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c88e642f20b40843dcd",
+    "url": "/notebook-react/static/js/main.e92aec79.chunk.js"
+  },
+  {
+    "revision": "60b9aa9d38e6bc378fe1",
+    "url": "/notebook-react/static/js/runtime-main.e1ed781c.js"
+  }
+]);
