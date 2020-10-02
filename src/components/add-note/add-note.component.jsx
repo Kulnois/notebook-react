b44a10486ec5e1503736b1dcadc8a6a8ee400746
Kulnois <rasecmen58@hotmail.com>
2020-10-02 14:09:01 -0500

    The logic to add a note is implemented

--- src/components/add-note/add-note.component.jsx
+++ src/components/add-note/add-note.component.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import useInput from '../../custom-hooks/use-input'
+import { addNote } from '../../redux/actions/note.action'
+import { useDispatch } from 'react-redux'
 
 const AddNote = () => {
+
+    const [ title, bindTitle, resetTitle ] = useInput()
+    const [ content, bindContent, resetContent ] = useInput()
+    const dispatch = useDispatch()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        dispatch(addNote({ title, content }))
+        resetTitle()
+        resetContent()
+    }
+
     return (
         <div>
             <div className="section">
-                <form action="#" className="white">
+                <form onSubmit={handleSubmit} className="white">
                     <h5 className="grey-text text-darken-3">New Note</h5>
                     <div className="input-field">
-                        <input id="note_title" type="text" className="validate"/>
+                        <input id="note_title" type="text" required className="validate" {...bindTitle}/>
                         <label htmlFor="note_title" className="active">Note Title</label>
                     </div>
                     <div className="input-field">
-                        <textarea id="note_content" rows="4" className="materialize-textarea"></textarea>
+                        <textarea id="note_content" rows="4" required className="materialize-textarea" {...bindContent}></textarea>
                         <label htmlFor="note_content">Note Content</label>
                     </div>
                     <button className="btn green">Add</button>
