7b60e9368ae1b232cef039ef56e2bb0b52832f57
Kulnois <rasecmen58@hotmail.com>
2020-10-02 12:07:09 -0500

    HomePage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Navbar from './layout/navbar'
+import HomePage from './pages/home/home.component'
 
 function App() {
   return (
     <>
       <Navbar />
+      <HomePage />
     </>
   );
 }
---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div className="container">
+            <div className="row center-align">
+                <div className="col m7">
+                    Form Add Note
+                </div>
+                <div className="col m5">
+                    List Note
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
